e4125e8d8d27aab9e7ea35daed09b664843c90fa
EC950 <jasonfu516@outlook.com>
2021-11-12 13:02:56 +0800

    commit

--- Hardware/ESP-AP/main/main.c
+++ Hardware/ESP-AP/main/main.c
@@ -2,10 +2,14 @@
 #include <nvs_flash.h>
 #include <esp_wifi.h>
 #include <esp_log.h>
+#include <esp_http_server.h>
+
+#define AP_ESP_WIFI_MAX_CONN 10
+#define AP_ESP_WIFI_SSID "ESP8266"
+#define AP_ESP_WIFI_PASS "Asdf1234"
+#define AP_ESP_WIFI_AUTH WIFI_AUTH_WPA_PSK
 
 static const char *TAG = "EXAMPLE_TAG";
-static const char *EXAMPLE_ESP_WIFI_SSID = "ESP8266";
-static const char *EXAMPLE_ESP_WIFI_PASS = "Asdf1234";
 
 static void wifi_event_handler(void *arg, esp_event_base_t event_base, int32_t event_id, void *event_data) {
     switch (event_id) {
@@ -28,31 +32,34 @@ static void wifi_event_handler(void *arg, esp_event_base_t event_base, int32_t e
 
 static void start_ap() {
     wifi_config_t wifi_config_ap = {.ap = {
-            .ssid = {EXAMPLE_ESP_WIFI_SSID},
-            .ssid_len = strlen(EXAMPLE_ESP_WIFI_SSID),
-            .password = {EXAMPLE_ESP_WIFI_PASS},
-            .max_connection = 10,
-            .authmode = WIFI_AUTH_WPA2_WPA3_PSK
+            .ssid = AP_ESP_WIFI_SSID,
+            .ssid_len = strlen(AP_ESP_WIFI_SSID),
+            .password = AP_ESP_WIFI_PASS,
+            .max_connection = AP_ESP_WIFI_MAX_CONN,
+            .authmode = AP_ESP_WIFI_AUTH
     }};
-
-    tcpip_adapter_init();
-    ESP_ERROR_CHECK(esp_event_loop_create_default());
 
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
-    // setting Wi-Fi AP info
-    ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_AP, &wifi_config_ap));
-    // register Wi-Fi event handler
-    ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &wifi_event_handler, NULL));
-    // set Wi-Fi mode to AP mode
     ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_AP));
-    // start Wi-Fi
+    ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_AP, &wifi_config_ap));
+    ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &wifi_event_handler, NULL));
     ESP_ERROR_CHECK(esp_wifi_start());
-
     ESP_LOGI(TAG, "start ap...");
 }
 
+static void start_httpd() {
+    httpd_config_t config = HTTPD_DEFAULT_CONFIG();
+    httpd_handle_t handle = NULL;
+    if (httpd_start(&handle, &config) == ESP_OK) {
+        httpd_uri_t uri_connect_wifi;
+        httpd_register_uri_handler(handle, &uri_connect_wifi)
+    }
+}
+
 void app_main() {
+    tcpip_adapter_init();
     ESP_ERROR_CHECK(nvs_flash_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
     start_ap();
 }
