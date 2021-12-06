43be2c662e4dc8b89f7e033b516ffed95ed1fc42
EC950 <jasonfu516@outlook.com>
2021-12-06 17:43:58 +0800

    commit

---
+++ JavaWeb/E9C50-IOT/src/main/java/com/fuxin/iot/controller/IotServerController.java
@@ -0,0 +1,42 @@
+package com.fuxin.iot.controller;
+
+import com.fuxin.iot.mqtt.sender.MqttMessageSender;
+import com.fuxin.iot.utils.DeviceStatus;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/iotServer")
+public class IotServerController {
+
+    @Value("${mqtt.topic.command}")
+    private String topicPublish;
+
+    private final DeviceStatus deviceStatus;
+    private final MqttMessageSender mqttMessageSender;
+
+    @Autowired
+    public IotServerController(DeviceStatus deviceStatus, MqttMessageSender mqttMessageSender) {
+        this.deviceStatus = deviceStatus;
+        this.mqttMessageSender = mqttMessageSender;
+    }
+
+    @GetMapping("/publishMessage")
+    public boolean publishMessage(
+            @RequestParam("payload") String payload
+    ) {
+        mqttMessageSender.send(topicPublish, payload);
+        return true;
+    }
+
+    @GetMapping("/status")
+    public String getStatus() {
+        return deviceStatus.getStatus();
+    }
+}
